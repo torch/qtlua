7594c740063c71b533cd2affd9b85213e2614e6d
Ronan Collobert <locronan@fb.com>
2014-10-06 18:22:04 -0700

    cmake: workaround luarocks limitation (concerning lua library in use)

--- cmake/FindLua.cmake
+++ cmake/FindLua.cmake
@@ -10,22 +10,83 @@
 
 INCLUDE(CheckLibraryExists)
 
-SET(LUA_EXECUTABLE "${LUA}")
+IF(LUA) # if we are using luarocks
+  MESSAGE(STATUS "Lua: using information from luarocks")
+  SET(LUA_EXECUTABLE "${LUA}")
+  SET(LUA_INCLUDE_DIR "${LUA_INCDIR}")
+  SET(LUA_PACKAGE_PATH "${LUADIR}")
+  SET(LUA_PACKAGE_CPATH "${LIBDIR}")
 
-FIND_LIBRARY(LUA_LIBRARIES
-  NAMES lua liblua luajit libluajit lua-5.1 luajit-5.1 liblua-5.1 libluajit-5.1
-  PATHS ${LUA_LIBDIR}
-  ${LUA_BINDIR}
-  NO_DEFAULT_PATH)
+  IF(LUALIB) # present on windows platforms only
+    SET(LUA_LIBRARIES "${LUALIB}")
+  ELSE() # too bad, luarocks does not provide it (pfff...)
+    GET_FILENAME_COMPONENT(LUA_EXEC_NAME ${LUA_EXECUTABLE} NAME_WE)
+    IF(LUA_EXEC_NAME STREQUAL "luajit")
+    FIND_LIBRARY(LUA_LIBRARIES
+      NAMES luajit libluajit
+      PATHS ${LUA_LIBDIR}
+      NO_DEFAULT_PATH)
+    ELSEIF(LUA_EXEC_NAME STREQUAL "lua")
+      FIND_LIBRARY(LUA_LIBRARIES
+        NAMES lua liblua
+        PATHS ${LUA_LIBDIR}
+        NO_DEFAULT_PATH)
+    ELSE()
+      MESSAGE(FATAL_ERROR "You seem to have a non-standard lua installation -- are you using luajit-rocks?")
+    ENDIF()
+    MESSAGE(STATUS "Lua library guess (no info from luarocks): ${LUA_LIBRARIES}")
+  ENDIF()
+
+ELSE() # standalone -- not using luarocks
+
+  IF(WITH_LUA51)
+    FIND_PROGRAM(LUA_EXECUTABLE
+      NAMES
+      lua51
+      lua5.1
+      lua
+      PATH)
+  ELSE()
+    FIND_PROGRAM(LUA_EXECUTABLE
+      NAMES
+      luajit
+      PATH)
+  ENDIF()
+  MESSAGE(STATUS "Lua executable: ${LUA_EXECUTABLE}")
+
+  IF(LUA_EXECUTABLE)
+    GET_FILENAME_COMPONENT(LUA_DIR ${LUA_EXECUTABLE} PATH)
+  ENDIF(LUA_EXECUTABLE)
+
+  IF(WITH_LUA51)
+    FIND_LIBRARY(LUA_LIBRARIES
+      NAMES lua liblua lua-5.1 liblua-5.1 libluajit-5.1
+      PATHS ${LUA_DIR}/../lib
+      ${LUA_DIR}
+      NO_DEFAULT_PATH)
+  ELSE()
+    FIND_LIBRARY(LUA_LIBRARIES
+      NAMES luajit libluajit luajit-5.1 libluajit-5.1
+      PATHS ${LUA_DIR}/../lib
+      ${LUA_DIR}
+      NO_DEFAULT_PATH)
+  ENDIF()
+  MESSAGE(STATUS "Lua library: ${LUA_LIBRARIES}")
+
+  FIND_PATH(LUA_INCLUDE_DIR lua.h
+    ${LUA_DIR}/../include/lua-5.1
+    ${LUA_DIR}/../include/lua51
+    ${LUA_DIR}/../include/lua
+    ${LUA_DIR}/../include
+    NO_DEFAULT_PATH)
+
+  MESSAGE(STATUS "Lua include directory: ${LUA_INCLUDE_DIR}")
+
+ENDIF()
 
 IF(LUA_LIBRARIES)
   CHECK_LIBRARY_EXISTS(${LUA_LIBRARIES} luaJIT_setmode "" LUA_JIT)
 ENDIF()
-
-SET(LUA_INCLUDE_DIR "${LUA_INCDIR}")
-
-SET(LUA_PACKAGE_PATH "${LUADIR}")
-SET(LUA_PACKAGE_CPATH "${LIBDIR}")
 
 MARK_AS_ADVANCED(
   LUA_EXECUTABLE
